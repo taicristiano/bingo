62b7347a64c10ff6b1d8913bf7cc4610a38dc738
TaiTD <taitd@rikkeisoft.com>
2018-01-04 15:22:37 +0700

    Up code

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,400 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title></title>
+    <link rel="stylesheet" href="">
+    <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="http://118.70.128.67:85/assets/css/icons/icomoon/styles.css?v=1503545922" type="text/css" >
+    <link rel="stylesheet" href="http://118.70.128.67:85/assets/css/icons/fontawesome/css/font-awesome.min.css?v=1503545922" type="text/css" >
+    <link rel="stylesheet" href="http://118.70.128.67:85/assets/css/core.css?v=1510295790" type="text/css" >
+    <link rel="stylesheet" href="http://118.70.128.67:85/assets/css/components.css?v=1509102884" type="text/css" >
+    <link rel="stylesheet" href="http://118.70.128.67:85/assets/css/colors.css?v=1503545921" type="text/css" >
+    <link rel="stylesheet" href="http://118.70.128.67:85/assets/css/jquery.alerts.css?v=1503545921" type="text/css" >
+    <link rel="stylesheet" href="http://118.70.128.67:85/assets/css/colorbox.css?v=1503545923" type="text/css" >
+    <link rel="stylesheet" href="http://118.70.128.67:85/assets/css/jquery-ui.css?v=1503545921" type="text/css" >
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+    <script src="http://118.70.128.67:85/assets/js/core/libraries/jquery_ui/jquery-ui.min.js?v=1503545964" type="text/javascript" charset="utf-8" ></script>
+    <script src="http://118.70.128.67:85/assets/js/plugins/loaders/blockui.min.js?v=1503545926" type="text/javascript" charset="utf-8" ></script>
+    <script src="http://118.70.128.67:85/assets/js/plugins/ui/nicescroll.min.js?v=1503545954" type="text/javascript" charset="utf-8" ></script>
+    <script src="http://118.70.128.67:85/assets/js/plugins/ui/drilldown.js?v=1503545954" type="text/javascript" charset="utf-8" ></script>
+    <script src="http://118.70.128.67:85/assets/js/plugins/ui/moment/moment.min.js?v=1503545954" type="text/javascript" charset="utf-8" ></script>
+    <script src="http://118.70.128.67:85/assets/js/common/jquery.utility.js?v=1503545960" type="text/javascript" charset="utf-8" ></script>
+    <script src="http://118.70.128.67:85/assets/js/common/utility.js?v=1507281528" type="text/javascript" charset="utf-8" ></script>
+    <script src="http://118.70.128.67:85/assets/js/common/msg.vi.js?v=1507283834" type="text/javascript" charset="utf-8" ></script>
+    <script src="http://118.70.128.67:85/assets/js/common/jquery.alerts.js?v=1503545960" type="text/javascript" charset="utf-8" ></script>
+    <script src="http://118.70.128.67:85/assets/js/common/jquery.colorbox.js?v=1503545959" type="text/javascript" charset="utf-8" ></script>
+    <script src="http://118.70.128.67:85/assets/js/common/jquery.stickytable.js?v=1508656736" type="text/javascript" charset="utf-8" ></script>
+    <script src="http://118.70.128.67:85/assets/js/plugins/forms/inputs/autoNumeric-min.js?v=1503545925" type="text/javascript" charset="utf-8" ></script>
+    <script src="http://118.70.128.67:85/assets/message/msg.js?v=1503550611" type="text/javascript" charset="utf-8" ></script>
+    <script src="http://118.70.128.67:85/assets/js/common/common.js?v=1514882340" type="text/javascript" charset="utf-8" ></script>
+    <script src="http://118.70.128.67:85/assets/js/common/config.js?v=1503545959" type="text/javascript" charset="utf-8" ></script>
+    <script src="http://118.70.128.67:85/assets/js/common/jquery-ui.js?v=1503545960" type="text/javascript" charset="utf-8" ></script>
+    <script src="http://118.70.128.67:85/assets/js/common/jquery.ympicker.js?v=1503545960" type="text/javascript" charset="utf-8" ></script>
+    <!-- Component JS files -->
+    <script src="http://118.70.128.67:85/assets/js/plugins/tables/fixheader/tableHeadFixer.js"></script>
+    <style>
+        body {
+            background: url(http://hinhnenpowerpointdep.com/uploads/images/tuyen-tap-30-hinh-nen-powerpoint-dep-tuyet-voi-ve-ngay-tet-1484150078-1.jpg) no-repeat center center fixed; 
+            -webkit-background-size: cover;
+            -moz-background-size: cover;
+            -o-background-size: cover;
+            background-size: cover;
+        }
+        .w-100-per {
+            width: 100%;
+        }
+        .margin-top-100 {
+            margin-top: 100px;
+        }
+        .btn_play button {
+            background-image: url(http://118.70.128.67:85/assets/images/bingo/btn_play6.png);
+            position: absolute;
+            width: 190px;
+            height: 190px;
+            top: 48px;
+            left: 88px;
+            border-radius: 120px;
+        }
+        #img_play {
+            width: 285px;
+            height: 285px;
+        }
+        .resultNumber {
+            list-style: none;
+            margin: 6px;
+            border-radius: 100px;
+            width: 113px;
+            height: 109px;
+            color: white;
+            font-weight: bold;
+            font-size: 40px;
+            text-align: center;
+            line-height: 109px;
+            background-image: url(http://118.70.128.67:85/assets/images/bingo/result2.png);
+            margin: 0 auto;
+        }
+        td {
+            text-align: center;
+        }
+        td.selected {
+            color: white !important;
+            font-size: 13px;
+            font-weight: bold;
+            /*background-color: red !important;*/
+            background: url(https://image.ibb.co/fzxWsG/result1.png);
+        }
+        .img_play {
+            -webkit-animation: rotation 1s infinite linear;
+        }
+        .margin-top-25 {
+            margin-top: 25px;
+        }
+        table,
+        tbody,
+        tr,
+        td {
+            border: none !important;
+        }
+        .table {
+            width: 450px !important;
+            height: 500px !important;
+        }
+
+        .table>tbody>tr>td, .table>tbody>tr>th, .table>tfoot>tr>td, .table>tfoot>tr>th, .table>thead>tr>td, .table>thead>tr>th {
+            padding: 8px;
+            line-height: 1.42857143;
+            vertical-align: middle;
+            background-color: #f2f2f2;
+            border-radius: 50%;
+            font-size: 22px;
+            color: #bfbfbf;
+            font-family: serif;
+        }
+    </style>
+</head>
+<body>
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-10 col-lg-offset-1 col-md-10 col-md-offset-1 col-sm-12 col-xs-12 content-bingo">
+                <div class="text-center" style="margin-bottom: 20px">
+                    <img src="https://drive.google.com/uc?export=view&id=1rwv1tOAXHTUWjrhXDOgM2y_AOw1L6qFs">
+                </div>
+                <div class="col-lg-7 col-md-7 col-sm-12 col-xs-12 table-bingo">
+                    <div class="tb_number">
+                        <table border="1" class="table">
+                            <tbody>
+                                <tr>
+                                    <td>1</td>
+                                    <td>1</td>
+                                    <td>1</td>
+                                    <td>1</td>
+                                    <td>1</td>
+                                    <td>1</td>
+                                    <td>1</td>
+                                    <td>1</td>
+                                    <td>1</td>
+                                </tr>
+                                <tr>
+                                    <td>2</td>
+                                    <td>2</td>
+                                    <td>2</td>
+                                    <td>2</td>
+                                    <td>2</td>
+                                    <td>2</td>
+                                    <td>2</td>
+                                    <td>2</td>
+                                    <td>2</td>
+                                </tr>
+                                <tr>
+                                    <td>3</td>
+                                    <td>3</td>
+                                    <td>3</td>
+                                    <td>3</td>
+                                    <td>3</td>
+                                    <td>3</td>
+                                    <td>3</td>
+                                    <td>3</td>
+                                    <td>3</td>
+                                </tr>
+                                <tr>
+                                    <td>4</td>
+                                    <td>4</td>
+                                    <td>4</td>
+                                    <td>4</td>
+                                    <td>4</td>
+                                    <td>4</td>
+                                    <td>4</td>
+                                    <td>4</td>
+                                    <td>4</td>
+                                </tr>
+                                <tr>
+                                    <td>5</td>
+                                    <td>5</td>
+                                    <td>5</td>
+                                    <td>5</td>
+                                    <td>5</td>
+                                    <td>5</td>
+                                    <td>5</td>
+                                    <td>5</td>
+                                    <td>5</td>
+                                </tr>
+                                <tr>
+                                    <td>6</td>
+                                    <td>6</td>
+                                    <td>6</td>
+                                    <td>6</td>
+                                    <td>6</td>
+                                    <td>6</td>
+                                    <td>6</td>
+                                    <td>6</td>
+                                    <td>6</td>
+                                </tr>
+                                <tr>
+                                    <td>7</td>
+                                    <td>7</td>
+                                    <td>7</td>
+                                    <td>7</td>
+                                    <td>7</td>
+                                    <td>7</td>
+                                    <td>7</td>
+                                    <td>7</td>
+                                    <td>7</td>
+                                </tr>
+                                <tr>
+                                    <td>8</td>
+                                    <td>8</td>
+                                    <td>8</td>
+                                    <td>8</td>
+                                    <td>8</td>
+                                    <td>8</td>
+                                    <td>8</td>
+                                    <td>8</td>
+                                    <td>8</td>
+                                </tr>
+                                <tr>
+                                    <td>9</td>
+                                    <td>9</td>
+                                    <td>9</td>
+                                    <td>9</td>
+                                    <td>9</td>
+                                    <td>9</td>
+                                    <td>9</td>
+                                    <td>9</td>
+                                    <td>9</td>
+                                </tr>
+                                <tr>
+                                    <td>10</td>
+                                    <td>10</td>
+                                    <td>10</td>
+                                    <td>10</td>
+                                    <td>10</td>
+                                    <td>10</td>
+                                    <td>10</td>
+                                    <td>10</td>
+                                    <td>10</td>
+                                </tr>
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
+                <div class="col-lg-5 col-md-5 col-sm-12 col-xs-12 play-and-result">
+                    <div class="col-lg-12 col-md-12 col-sm-8 col-xs-8 text-center play-button">
+                        <div class="btn_play">
+                            <img id="img_play" src="http://118.70.128.67:85/assets/images/bingo/bg_btn_play1.png"
+                            class="img_btnBgBingo"/>
+                            <button id="btn_play" class="btnPlay playLg"></button>
+                        </div>
+                    </div>
+                    <div class="col-lg-12 col-md-12 col-sm-4 col-xs-4 margin-top-25 result-bingo">
+                        <div class="com">
+                            <div id="number_2" class="resultNumber n2"></div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- <iframe src="https://www.nhaccuatui.com/mh/background/4UhjQW_4Gt" width="1" height="1" frameborder="0" allowfullscreen></iframe> -->
+    <script>
+        /**
+         * Created by Administrator on 2017/10/19.
+         */
+        'use strict';
+        var project_no = '';
+        var employee_no = '';
+        var position = '';
+        var audioElement;
+        var audioBg;
+        $(document).ready(function () {
+            var width = $('#img_play').width();
+            var height = $('#img_play').height();
+            initEvents();
+            setPosition();
+            //initControls();
+        });
+        $( window ).resize(function() {
+            setPosition();
+        });
+        function music_bingo() {
+            audioBg = document.getElementById('music_bg');
+            audioElement = document.createElement('audio');
+            audioElement.setAttribute('src','/home/taitd/Desktop/XoSoMienBacNhacChuong-VA-4937390.mp3');
+            audioElement.addEventListener('ended', function() {
+                this.play();
+            }, false);
+        
+            if(audioElement.play()) {
+                //var a = audioBg.pause();
+                /*audioElement.currentTime = 3;*/
+            }
+           /* if(parseInt(audioElement.currentTime) == 2) {
+                audioBg.play();
+            }
+            if(audioElement.pause()) {
+                audioBg.play();
+            }*/
+        }
+        function initEvents() {
+            var bingo = {
+                selectedNumbers: [],
+                generateRandom: function() {
+                    var min = 1;
+                    var max = 90;
+                    var random = Math.floor(Math.random() * (max - min + 1)) + min;
+                    return random;
+                },
+                generateNextRandom: function() {
+        
+                    var random = bingo.generateRandom();
+                    while ($.inArray(random, bingo.selectedNumbers) > -1) {
+                        random = bingo.generateRandom();
+                    }
+                    bingo.selectedNumbers.push(random);
+                    return random;
+                }
+            };
+        
+            $(document).on('click', '#btn_play', function (e) {
+                var $this = $(this);
+                if ($(this).data('running')) {
+                    return;
+                }
+                $(this).data('running', true);
+                $(this).attr('disabled', 'disabled');
+                // music_bingo();
+                if(bingo.selectedNumbers.length > 89){
+                    alert("All number has been generated");
+                    return false;
+                }
+        
+                $("#img_play").addClass("img_play");
+        
+                setTimeout(function () {
+                    $('#btn_play').data('running', false);
+                    $("#img_play").removeClass("img_play");
+                    $('#btn_play').removeAttr('disabled');
+        
+                    //generate random number
+                    var random1 = bingo.generateNextRandom().toString();
+                  /*  var random2 = bingo.generateNextRandom().toString();
+                    var random3 = bingo.generateNextRandom().toString();*/
+        
+                    $('td.cell' + random1).addClass('selected');
+                    /*$('td.cell' + random2).addClass('selected');*/
+                    /*$('td.cell' + random3).addClass('selected');*/
+        
+                    //set value to result
+                    // audioElement.pause();
+                    // audioElement.currentTime = 0;
+                   // $("audio").currentTime = 0;
+                    $("#number_2").text(random1);
+                    /*$("#number_2").text(random2);*/
+                   /* $("#number_3").text(random3);*/
+                    // audioBg.play();
+        
+                }, 1000)
+            });
+        
+            resignNumber();
+        
+        
+        }
+        
+        function resignNumber()
+        {
+            $('td').each(function() {
+                var concatClass = this.cellIndex + "" + this.parentNode.rowIndex;
+                var numberString = (parseInt(concatClass, 10)+1).toString();
+                if(numberString*1>99){
+                    $(this).addClass("cell" + numberString).text('');
+                } else {
+                    $(this).addClass("cell" + numberString).text(numberString);
+                }
+            });
+        }
+
+        function setPosition()
+        {
+            var height = $('body').height();
+            var contentHeight = $('.content-bingo').height();
+            var marginTop = (height - contentHeight) / 2;
+            console.log(marginTop);
+            $('.content-bingo').css('margin-top', marginTop + 'px');
+
+            var heightTable = $('.table-bingo').height();
+            var helghtPlayButton = $('.play-button').height();
+            var helghtResult = $('.result-bingo').height();
+            $('.play-and-result').css('height', heightTable + 'px');
+            var marginTopBingo = (heightTable - helghtPlayButton - helghtResult) / 3;
+            $('.play-button').css('margin-top', marginTopBingo + 'px');
+            $('.result-bingo').css('margin-top', marginTopBingo + 'px');
+        }
+        window.onbeforeunload = function() {
+            return true;
+        };
+    </script>
+</body>
+</html>
